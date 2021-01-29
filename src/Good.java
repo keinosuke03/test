7740d5f03d1bb778c5c778e79624a06d64b61e7f
keinosuke03 <nosuke0903-db3@keio.jp>
2021-01-29 22:20:19 +0900

    Add messages.

--- src/Good.java
+++ src/Good.java
@@ -1,5 +1,7 @@
 public class Good{
 	public static void main(String[] atgs){
-		System.out.println("Good!");
+		System.out.println("Good!!!!!!!");
+		System.out.println("Try!Try!");
+		System.out.println("Good!Good!");
 	}
 }
